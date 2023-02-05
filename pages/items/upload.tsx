975e2a23a5e6b3b23e2aa92dbdc5fcc701249a4b
김재준 <kimsman@gimjaejun-ui-MacBookPro.local>
2023-02-05 16:13:16 +0900

    Upload pages

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -56,7 +56,7 @@ const Upload: NextPage = () => {
         </div>
       </div>
       <button className="mt-5 w-full bg-orange-500 hover:bg-ornage-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product.
+        Upload product
       </button>
     </div>
   );
